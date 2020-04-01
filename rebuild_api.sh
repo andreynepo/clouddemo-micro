fcfc1b1ab8a9b58504953f9c12df4630c082d9f2
andreynepo <andreynepo@gmail.com>
2020-04-01 07:38:42 +0000

    fixed TNS_ADMIN env variable

--- rebuild_api.sh
+++ rebuild_api.sh
@@ -2,13 +2,8 @@
 echo NAMESPACE: $NAMESPACE
 if [ $NAMESPACE != "" ]
 then
-  if [ -d clouddemo-api/docker/context/.oci ]
-  then
-    echo Building image...
-    docker build -t fra.ocir.io/$NAMESPACE/clouddemo-micro/api clouddemo-api/docker/
-  else
-    echo "Error: .oci not found."
-  fi
+  echo Building image...
+  docker build -t fra.ocir.io/$NAMESPACE/clouddemo-micro/api clouddemo-api/docker/
 else
   echo "Error: NAMESPACE env variable not set."
 fi
--- rebuild_db.sh
+++ rebuild_db.sh
@@ -2,13 +2,8 @@
 echo NAMESPACE: $NAMESPACE
 if [ $NAMESPACE != "" ]
 then
-  if [ -d clouddemo-db/docker/context/.wallet ]
-  then
-    echo Building image...
-    docker build -t fra.ocir.io/$NAMESPACE/clouddemo-micro/db clouddemo-db/docker/
-  else
-    echo "Error: .wallet not found."
-  fi
+  echo Building image...
+  docker build -t fra.ocir.io/$NAMESPACE/clouddemo-micro/db clouddemo-db/docker/
 else
   echo "Error: NAMESPACE env variable not set."
 fi
