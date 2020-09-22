c572c3aabe599dbbaaabdbb3b680db18eb5392f3
andreynepo <andreynepo@gmail.com>
2020-09-22 17:52:40 +0300

    rebuild for ocr microservice

---
+++ scripts/rebuild_ocr.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+echo NAMESPACE: $NAMESPACE
+echo REGION: $REGION
+if [ $NAMESPACE != "" ] && [ $REGION != "" ]
+then
+  echo Building image...
+  docker build -t $REGION.ocir.io/$NAMESPACE/clouddemo-micro/ocr clouddemo-ocr/docker/
+else
+  echo "Usage:"
+  echo "export NAMESPACE=<YOURNAMESPACE>"
+  echo "export REGION=<YOURREGION>"
+  echo "./rebuild_api.sh"
+fi
